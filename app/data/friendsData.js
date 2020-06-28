42ee214622d2ebe6e9fe8d14d94bbd3a5e2c0dce
Curtis Lane <clanelmt@gmail.com>
2020-06-28 15:27:07 -0400

    add friendData for three fake examples

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,7 +1,52 @@
 let friendsArr = [
     {
-        // Friend name, image, and survey answers
-    }
+        name: 'Real Actual Human',
+          photo: 'https://i.guim.co.uk/img/media/3aab8a0699616ac94346c05f667b40844e46322f/0_123_5616_3432/master/5616.jpg?width=445&quality=85&auto=format&fit=max&s=f36ad0687e104725c985cdf26bfb402e',
+          scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+          ]
+    },
+    {
+        name: 'Yornu Besfren',
+          photo: 'https://e3.365dm.com/20/02/768x432/skynews-justin-bieber-youtube_4911191.jpg?20200204120355',
+          scores: [
+            5,
+            4,
+            3,
+            4,
+            2,
+            1,
+            4,
+            5,
+            3,
+            5
+          ]
+    },
+    {
+        name: 'Notta Stalker',
+          photo: 'https://i.kym-cdn.com/entries/icons/facebook/000/010/496/Overly_attached_GF.jpg',
+          scores: [
+            1,
+            2,
+            4,
+            2,
+            4,
+            5,
+            2,
+            1,
+            4,
+            1
+          ]
+    },
 ]
 
 module.exports = friendsArr;
