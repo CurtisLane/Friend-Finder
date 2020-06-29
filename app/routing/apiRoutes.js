ef94805978f15c717f03b1601ee0439763cddd77
Curtis Lane <clanelmt@gmail.com>
2020-06-28 20:42:51 -0400

    complete logic for friend match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,29 @@ module.exports = function(app){
     })
 
     app.post('/api/friends', function(req, res){
-        friendsData.push(req.body);
-        res.json(friendsData)
+        let userData = req.body
+        let friendMatch = {
+            name: '',
+            photo: '',
+            diff: 10000
+        }
+
+        let compare = friendsData.map(function(friend){
+            let totalDiff = 0;
+            for (let i = 0; i<friend.scores.length;i++){
+                totalDiff += Math.abs(parseInt(userData.scores[i]) - parseInt(friend.scores[i]))   
+            }
+            if (totalDiff<= friendMatch.diff){
+                friendMatch.name=friend.name;
+                friendMatch.photo=friend.photo;
+                friendMatch.diff=totalDiff;
+            }
+            
+            return friendMatch
+        })
+
+        // friendsData.push(userData); // If we want to add the userData to our friendData array
+
+        res.json(compare);
     })
 }
