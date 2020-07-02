4014f6723566651ea269f22c4b3a15564ff8c0b6
Curtis Lane <clanelmt@gmail.com>
2020-07-02 12:33:00 -0400

    update friendData, change one photo, change survey answers from test to real case.

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,80 +1,80 @@
 let friendsArr = [
     {
-        name: 'Real Actual Human',
+        name: 'Harrold Arnot',
           photo: 'https://i.guim.co.uk/img/media/3aab8a0699616ac94346c05f667b40844e46322f/0_123_5616_3432/master/5616.jpg?width=445&quality=85&auto=format&fit=max&s=f36ad0687e104725c985cdf26bfb402e',
           scores: [
+            3,
             5,
+            2,
+            1,
             5,
+            3,
             5,
+            4,
             5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5
+            1
           ]
     },
     {
-        name: 'Yornu Besfren',
+        name: 'Bustin Jieber',
           photo: 'https://e3.365dm.com/20/02/768x432/skynews-justin-bieber-youtube_4911191.jpg?20200204120355',
           scores: [
             5,
-            4,
-            3,
-            4,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1
+          ]
+    },
+    {
+        name: 'Samatha Stalkyr',
+          photo: 'https://i.kym-cdn.com/entries/icons/facebook/000/010/496/Overly_attached_GF.jpg',
+          scores: [
+            1,
+            1,
+            1,
+            1,
             2,
             1,
-            4,
+            1,
             5,
-            3,
+            5,
             5
           ]
     },
     {
-        name: 'Notta Stalker',
-          photo: 'https://i.kym-cdn.com/entries/icons/facebook/000/010/496/Overly_attached_GF.jpg',
+        name: 'Allan Peterson',
+          photo: 'https://talesfromtherahttps://p0.piqsels.com/preview/456/694/541/4fa6mtdlv1qg59j2h7f1qgnpqv-thumbnail.jpgdiationage.files.wordpress.com/2014/01/kodos.jpg',
           scores: [
-            1,
-            2,
-            4,
-            2,
-            4,
             5,
-            2,
             1,
-            4,
+            3,
+            1,
+            3,
+            5,
+            5,
+            5,
+            5,
             1
           ]
     },
     {
-        name: 'Amnot Analian',
-          photo: 'https://talesfromtheradiationage.files.wordpress.com/2014/01/kodos.jpg',
-          scores: [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
-          ]
-    },
-    {
-        name: 'Fortes Tingpurpose',
+        name: 'Tabitha Tester',
           photo: 'https://www.adventhealth.com/sites/default/files/styles/fc_800x533/public/media/hero-woman-testing-sample-2000x1333.jpg?h=1d555a86&itok=9bNqsC0-',
           scores: [
             3,
             3,
+            5,
             3,
             3,
             3,
             3,
-            3,
-            3,
+            2,
             3,
             3
           ]
